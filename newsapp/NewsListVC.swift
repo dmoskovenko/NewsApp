3ae9dab4cff5058088d960a856d5c9d4322a70db
Dmitry <moskovenko.dmitry@yandex.ru>
2020-08-07 13:19:27 +0300

    initial commit

---
+++ newsapp/NewsListVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewsListVC.swift
+//  newsapp
+//
+//  Created by dima on 07/08/2020.
+//  Copyright © 2020 Dmitry Moskovenko. All rights reserved.
+//
+
+import UIKit
+
+class NewsListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
